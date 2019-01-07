02724a1a94636c6dce357c101fe39deae889600d
Sai Krishna <srallaba@cs.cmu.edu>
2019-01-07 14:12:58 -0500

    Create get_stuff.sh

---
+++ bin/get_stuff.sh
@@ -0,0 +1,11 @@
+
+## This is not an automatic script yet. Just to illustrate the steps
+
+# Clone the repos fetival, festvox, speech tools.
+
+wget http://festvox.org/packed/festival/2.5/festlex_CMU.tar.gz
+wget http://festvox.org/packed/festival/2.5/festlex_OALD.tar.gz
+wget http://festvox.org/packed/festival/2.5/festlex_POSLEX.tar.gz
+wget http://tts.speech.cs.cmu.edu/awb/festival-2.5/voices/festvox_kallpc16k.tar.gz
+wget http://tts.speech.cs.cmu.edu/awb/festival-2.5/voices/festvox_rablpc16k.tar.gz
+wget http://tts.speech.cs.cmu.edu/rsk/vc_stuff/SPTK-3.9.clone.tar.gz
