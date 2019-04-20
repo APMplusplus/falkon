a22ee72079d92d53ab146f1ff267971dd43c4c99
Sai Krishna <srallaba@cs.cmu.edu>
2019-04-20 16:40:07 -0400

    Create run.sh

---
+++ paradigms/variational_inference/vqvae/run.sh
@@ -0,0 +1,6 @@
+
+
+# Download preprocessed data
+wget http://tts.speech.cs.cmu.edu/rsk/impactathons/april_2019/data/dataprep_vcc2018_vqvae_19april2019.tar.gz
+
+# Run the baseline from local/baseline*
