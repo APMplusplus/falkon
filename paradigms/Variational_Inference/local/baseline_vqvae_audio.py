1ab3963ef0534412477d8edd2830e3d346b7a79a
saikrishnarallabandi <srallaba@cs.cmu.edu>
2019-04-20 01:15:57 +0000

    Added barebones script for VQVAE

---
+++ paradigms/Variational_Inference/local/baseline_vqvae_audio.py
@@ -0,0 +1,24 @@
+'''
+
+Script for baseline implementation of Vector Quantized Variational AutoEncoders
+This is for speech generation. For now, its implemented on Voice Conversion 2018 dataset
+
+'''
+
+## Imports
+import os, sys
+import logging
+
+from utils import *
+
+## Flags and locations
+FALCON_DIR = os.environ.get('FALCON_DIR')
+data_dir = /home/srallaba/development/repos/falkon/paradigms/Variational_Inference/data/data_vcc2018_multispk/
+print_flag = 1
+
+## Train loop
+
+## Eval loop
+
+## Main thingy
+
---
+++ paradigms/Variational_Inference/local/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import multiprocessing as mp
+
+
+def get_files(data_dir):
+
+   
+
+def preprocess_data(data_dir = './data/vcc2018'):
+
+   
+
+
+   return data_dir
+
